7b5033892a7f38d8949d39a95cdec62d3daefc9b
Ethan Xue <ethan.xue@gmail.com>
2018-06-16 19:39:29 -0400

    quick select

---
+++ quick-select.py
@@ -0,0 +1,6 @@
+# Given an array, finds the element that would be at position k of the sorted array
+# basically the partition algorithm used in quicksort
+# O(n) time
+
+def quick_select(array):
+    pass
