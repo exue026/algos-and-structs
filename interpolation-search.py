2e2f3ace82bba20fa3bce755aa5eb20b7b4a8ffa
Ethan Xue <ethan.xue@gmail.com>
2018-06-18 22:17:05 -0400

    nice

--- interpolation-search.py
+++ interpolation-search.py
@@ -1,3 +1,11 @@
 
-def interpolation_search():
-    pass
+def interpolation_search(array, k):
+    def recur(low, right):
+        if array[low] > k or array[high] < k:
+            return False
+        if low == high:
+            return array[low] == k
+        index = low + ((k - array[low]) / (array[high] - array[low])) * (high - low)
+        if array[index] == k
+
+            
