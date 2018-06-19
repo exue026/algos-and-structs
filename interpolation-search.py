3f0ac16b003aa6b3a09a328f99062a4a912ebc2e
Ethan Xue <ethan.xue@gmail.com>
2018-06-18 22:21:11 -0400

    done interpolation search

--- interpolation-search.py
+++ interpolation-search.py
@@ -1,11 +1,17 @@
 
 def interpolation_search(array, k):
-    def recur(low, right):
+    def recur(low, high):
         if array[low] > k or array[high] < k:
             return False
         if low == high:
             return array[low] == k
         index = low + ((k - array[low]) / (array[high] - array[low])) * (high - low)
-        if array[index] == k
+        if array[index] == k:
+            return True
+        if array[index] > k:
+            return recur(low, index - 1)
+        return recur(index + 1, high)
+    return recur(0, len(array) - 1)
 
+print(interpolation_search([1,2,3,4,5,6,7], 10))
             
